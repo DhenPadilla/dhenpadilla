3183882a41aa0e36cce16076f9c913212f76a86d
DhenPadilla <dhenepadilla@gmail.com>
2025-05-12 14:08:56 -0700

    remove dhenpadilla

--- dhenpadilla/src/app/writing/[notionId]/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page({
-    params,
-    searchParams,
-  }: {
-    params: Promise<{ notionId: string }>
-    searchParams: Promise<{ [key: string]: string | string[] | undefined }>
-  }) {
-    return <h1>My Page</h1>
-  }
