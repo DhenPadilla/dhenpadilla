3e62dd701b33674bd2403a3c0c955778ea48708d
DhenPadilla <dhenepadilla@gmail.com>
2025-05-15 17:30:07 -0700

    Favicon

--- dhenpadilla/src/app/layout.tsx
+++ dhenpadilla/src/app/layout.tsx
@@ -18,6 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon/dhpad.png" sizes="any" />
+
+      </head>
       <body
         className={`antialiased`}
         style={{ fontFamily: 'Times New Roman' }}
