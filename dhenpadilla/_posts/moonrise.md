cb2ac1e37852e8be9d1ba9198191d66d8aa4fdca
DhenPadilla <dhenepadilla@gmail.com>
2025-07-14 15:04:38 +0100

    moonrise

---
+++ dhenpadilla/_posts/moonrise.md
@@ -0,0 +1,35 @@
+---
+title: "moonrise"
+excerpt: ""
+coverImage: "/assets/blog/dynamic-routing/cover.jpg"
+date: "2025-08-14T13:35:00.000Z"
+author:
+  name: Dhen Padilla
+slug: moonrise
+audio: "/assets/audio/sea.mp3"
+audioTitle: "first moonrise.m4a (recorded 2025-07-13T00:02:45.080Z+02:00)"
+---
+
+slice softly, the sky and the
+sea. sand dunes
+carry the water.
+
+slide your way.
+bleed up. in
+to me. the air
+
+they have companion now,
+vincent.
+
+sister golden hair
+lies with morpheus.
+you guard the night shifts.
+
+lace my ankles with warm ice
+in sequence. in flow
+her laugh
+
+from left lung 
+to shoulder &mdash;
+the weight of the moon.
+her head.
--- dhenpadilla/src/app/cv/page.tsx
+++ dhenpadilla/src/app/cv/page.tsx
@@ -7,15 +7,16 @@ export default async function CV() {
     <main>
       <div className="flex flex-col max-h-[100vh] gap-4 max-w-3xl px-6 md:px-[80px] w-[100vw] max-w-[100vw] m-auto h-[100vh]">
         <article className="flex flex-col md:flex-row w-full gap-10">
-            <div className="flex flex-row md:flex-col sticky md:h-[100vh] py-[40px] md:w-[24%] w-full">
+            <div className="flex flex-row md:flex-col sticky md:h-[100vh] py-[56px] md:w-[24%] w-full">
                 <div className="text-md font-light italic">
                     Dhen Padilla (b. 1998) is a software engineer and artist based in London and New York.
-                    His engineering practice is concerned with building interfaces that solve social issues and also a volunteer contributor to <Link href="https://silk.cx" className="underline">Silk</Link>.
+                    <br/>
+                    His engineering practice is concerned with social issues, and reinvention. He is also a volunteer contributor to multiple open source projects and the creative platform, <Link href="https://silk.cx" className="underline">Silk</Link>.
                     <br />
                     Whilst his artistic work is grounded in transitivity, and is focused on the morality and liminality in humanity.
                 </div>
             </div>
-            <div className="flex flex-col w-full md:w-1/2 md:pt-[60px] gap-16 text-left overflow-y-auto">
+            <div className="flex flex-col w-full md:w-1/2 md:ml-[70px] md:pt-[60px] gap-16 text-left overflow-y-auto">
                 <HomeSection title="Technical">
                     <ul className="flex flex-col gap-2">
                         <li className="flex flex-row w-full justify-between">
@@ -44,16 +45,16 @@ export default async function CV() {
                     <ul className="flex flex-col gap-2">
                         <li className="flex flex-row w-full justify-between">
                             <span className="text-sm/[1.25em]">11.2024</span>
-                            <span className="ml-2 text-sm/[1.25em] italic">safaricamp, Oakland, CA</span>
+                            <span className="ml-2 text-sm/[1.25em] italic">Untitled &mdash; Group show, safaricamp, Oakland, CA</span>
                         </li>
                         <li className="flex flex-row w-full justify-between">
                             <span className="text-sm/[1.25em]">05.2024</span>
-                            <span className="ml-2 text-sm/[1.25em] italic">Lion's Den, San Francisco, CA</span>
+                            <span className="ml-2 text-sm/[1.25em] italic">Meet me in SF &mdash; Group show, Lion's Den, San Francisco, CA</span>
                         </li>
                     </ul>
                 </HomeSection>
                 <HomeSection title="Education">
-                    <ul className="flex flex-col gap-2">
+                    <ul className="flex flex-col gap-2 pb-20 md:pb-4">
                         <li className="flex flex-row w-full justify-between">
                             <span className="text-sm/[1.25em]">2016-2020</span>
                             <span className="ml-2 text-sm/[1.25em] italic">Master of Engineering, Computer Science, University College London</span>
